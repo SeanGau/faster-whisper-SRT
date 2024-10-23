463575681ad647f3922703fde838542a6fce72e2
SeanGau <rrtw0627@gmail.com>
2024-10-23 15:04:36 +0800

    split text by comma or 逗號 into multiple lines

--- whisper.py
+++ whisper.py
@@ -3,14 +3,18 @@ from pathlib import Path
 import opencc
 import argparse
 
-# 解析命令列參數
+# parse command line arguments
 parser = argparse.ArgumentParser(
     description="Transcribe an audio file using Whisper and save as SRT."
 )
 parser.add_argument("filename", type=str, help="Path to the audio file")
 parser.add_argument("--model", type=str, help="Model to use", default="large-v3")
-parser.add_argument("--language", type=str, help="Language of the audio file", default="zh")
-parser.add_argument("--keywords", type=str, help="Initial prompt for the model", default="")
+parser.add_argument(
+    "--language", type=str, help="Language of the audio file", default="zh"
+)
+parser.add_argument(
+    "--keywords", type=str, help="Initial prompt for the model", default=""
+)
 args = parser.parse_args()
 print(args)
 
@@ -20,14 +24,16 @@ filename = args.filename
 converter = opencc.OpenCC("s2tw")
 
 # Transcribe the audio file
-initial_prompt = f"This is a conversation about: 生成式 AI, ChatGPT, Claude AI, {args.keywords}"
+initial_prompt = (
+    f"This is a conversation about: 生成式 AI, ChatGPT, Claude AI, {args.keywords}"
+)
 segments, info = model.transcribe(
     filename,
     language=args.language,
     initial_prompt=initial_prompt,
     word_timestamps=True,
     vad_filter=True,
-    vad_parameters={"min_silence_duration_ms": 1000},
+    vad_parameters={"min_silence_duration_ms": 500},
 )
 
 
@@ -43,12 +49,21 @@ def format_timestamp(seconds):
 # Save SRT
 srt_file_path = Path(filename + ".srt")
 with open(srt_file_path, "w", encoding="utf-8") as srt_file:
+    id = 1
     for i, segment in enumerate(segments, start=1):
         start = format_timestamp(segment.start)
-        end = format_timestamp(segment.end)
-        text = converter.convert(segment.text)
-        line = f"{i}\n{start} --> {end}\n{text}"
-        print(line, flush=True)
-        srt_file.write(f"{line}\n\n")
+        current_text = ""
+        for word in segment.words:
+            current_text += word.word
+            end = format_timestamp(word.end)
+            # split text by comma or 逗號 into multiple lines
+            if "," in word.word or "，" in word.word or word == segment.words[-1]:
+                current_text = current_text.replace(",", "").replace("，", "").strip()
+                line = f"{id}\n{start} --> {end}\n{current_text}"
+                print(line, flush=True)
+                srt_file.write(f"{line}\n\n")
+                start = format_timestamp(word.end)
+                current_text = ""
+                id += 1
 
 print(f"SRT file saved to {srt_file_path}")
